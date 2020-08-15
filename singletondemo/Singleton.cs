c5879e77a12045c9ed245b24db0467dfbb603f1a
sarathkumarc <cmsarath123@gmail.com>
2020-08-15 20:25:23 +0530

    singleton single thread safe with lock keyword

---
+++ singletondemo/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace singletondemo
+{
+    public class Singleton
+    {
+        private static int ObjectsCount = 0;
+        public static Singleton instance = null;
+        private static readonly object padlock = new object();
+        public static Singleton GetInstance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                        instance = new Singleton();
+                    return instance;
+                }
+            }
+        }
+        private Singleton()
+        {
+            ObjectsCount++;
+            Console.WriteLine($"instances {ObjectsCount}");
+        }
+
+        public void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
